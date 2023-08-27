c467c20f6976afd46e9026108ff4dc626fc44ca7
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-08-27 16:37:37 +0530

    #1 - sidebar added

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,10 @@
+const ServerPage = () => {
+    return (
+        <div>
+            <h1>Server ID Page</h1>
+        </div>
+    )
+}
+
+
+export default ServerPage;
---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,28 @@
+"use client"
+import { CreateServerModal } from "@/components/modals/create-server-modal"
+import { useState ,useEffect } from "react"
+import { InviteModal } from "@/components/modals/invite-modal"
+import { EditServerModal } from "@/components/modals/edit-server-modal"
+import { MembersModal } from "@/components/modals/members-modal"
+
+export const ModalProvider = () => {
+const [isMounted, setIsMounted] = useState(false)
+
+useEffect(() => {
+    setIsMounted(true)
+},[])
+
+if(!isMounted){
+    return null
+}
+
+
+    return (
+        <>
+            <CreateServerModal />
+            <InviteModal/>
+            <EditServerModal/>
+            <MembersModal/>
+        </>
+    )
+}
